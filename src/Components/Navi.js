08b9d04e20cf4879e660e9698b7c9c11eff5cd5a
domitalk <dominic.m.chu@gmail.com>
2020-09-30 16:09:18 -0400

    scss setup

---
+++ src/Components/Navi.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navi() {
+    return (
+        <>
+            <div>
+                navi
+            </div>
+        </>
+    )
+}
+export default Navi;
